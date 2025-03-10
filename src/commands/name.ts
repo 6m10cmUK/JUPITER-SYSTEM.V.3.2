953964a2e4e79e1e615f995914f6e7f57996f649
6m10cmUK <jupity.610@gmail.com>
2025-03-10 12:52:37 +0900

    nameコマンド追加

---
+++ src/commands/name.ts
@@ -0,0 +1,68 @@
+import { 
+    ChatInputCommandInteraction, 
+    SlashCommandBuilder, 
+    SlashCommandIntegerOption,
+    SlashCommandStringOption,
+    EmbedBuilder
+} from 'discord.js';
+import { Command } from '../interfaces/Command';
+import fs from 'fs';
+import path from 'path';
+import { rollDice } from '../commons/dice';
+
+export const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName('name')
+        .setDescription('ランダム名前')
+        .addStringOption((option: SlashCommandStringOption) =>
+            option.setName('type')
+                .setDescription('表示する名前の種類')
+                .setRequired(true)
+                .addChoices(
+                    { name: '男性名', value: 'male' },
+                    { name: '女性名', value: 'female' }
+                )
+        )
+        .addIntegerOption((option: SlashCommandIntegerOption) =>
+            option.setName('count')
+                .setDescription('取得する名前の数')
+                .setMinValue(1)
+                .setMaxValue(3)
+                .setRequired(false)
+        )as SlashCommandBuilder,
+
+    async execute(interaction: ChatInputCommandInteraction) {
+
+        
+        const dataPath = path.join(process.cwd(), 'src', 'data', 'names.json');
+        const nameData = await JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+
+        const embed = new EmbedBuilder()
+        .setTitle('ランダム名前')
+        .setAuthor({
+            name: interaction.user.displayName,
+            iconURL: interaction.user.displayAvatarURL()
+        })
+        .setColor(0x888888);
+
+        const type = interaction.options.getString('type');
+        const count = interaction.options.getInteger('count') ?? 1;
+
+        console.log(count);
+        for (let i = 0; i < count; i++) {
+            const randomSei = nameData.sei[rollDice(1, 100).reduce((a, b) => a + b, 0) - 1];
+            let randomMei = '';
+            if (type === 'male') {
+                randomMei = nameData.mei.male[rollDice(1, 100).reduce((a, b) => a + b, 0) - 1];
+            } else {
+                randomMei = nameData.mei.female[rollDice(1, 100).reduce((a, b) => a + b, 0) - 1];
+            }
+
+            embed.addFields(
+                { name: `${randomSei}${randomMei}`, value: '' }
+            )
+        }
+
+        await interaction.reply({ embeds: [embed] });
+    }
+};
